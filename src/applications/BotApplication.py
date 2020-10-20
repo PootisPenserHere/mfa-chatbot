683889f83327ffd2df56ad25ed78d7bee54dd73b
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2020-10-19 21:40:44 -0600

    [mod] Instantiating the telegram bot

--- src/applications/BotApplication.py
+++ src/applications/BotApplication.py
@@ -5,9 +5,9 @@ from src.domain import environments
 
 class BotApplication:
     def __init__(self):
-        self.bot = telegram.bot(environments.TELEGRAM_TOKEN)
+        self.bot = telegram.Bot(environments.TELEGRAM_TOKEN)
 
     def respond_to_webhook(self, payload):
         print(payload)
-        update = telegram.update.Update.de_json(payload)
+        update = telegram.update.Update.de_json(payload, self.bot)
         self.bot.sendMessage(chat_id=update.message.chat_id, text='Hello, there')
