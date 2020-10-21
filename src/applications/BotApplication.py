8ca9086d6653c1ba9b58c6f235e6f6a17dab8ce8
Jose Pablo Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2020-10-20 21:07:55 -0600

    [add] The webhook response is handled by the application layer

--- src/applications/BotApplication.py
+++ src/applications/BotApplication.py
@@ -5,5 +5,6 @@ class BotApplication:
     def __init__(self, provider: str):
         self.chat_provider = ChatProvidersFactoryModel.ChatProvidersFactoryModel().create(chat_provider_type=provider)
 
-    def respond_to_webhook(self, payload):
-        self.chat_provider.respond_to_webhook(payload=payload)
+    def respond_to_webhook(self, payload: object):
+        print(f"response {payload['message']['text']}")
+        self.chat_provider.respond_to_webhook(payload=payload, message=payload['message']['text'])
--- src/models/TelegramModel.py
+++ src/models/TelegramModel.py
@@ -7,6 +7,6 @@ class TelegramModel:
     def __init__(self):
         self.bot = telegram.Bot(environments.TELEGRAM_TOKEN)
 
-    def respond_to_webhook(self, payload):
+    def respond_to_webhook(self, payload, message):
         update = telegram.update.Update.de_json(payload, self.bot)
-        self.bot.sendMessage(chat_id=update.message.chat_id, text=payload['message']['text'])
+        self.bot.sendMessage(chat_id=update.message.chat_id, text=message)
